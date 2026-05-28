aac0e8af958b2bcec30e408af4efda9f147c467f
ur.z3yn <unczayn@gmail.com>
2026-05-29 03:14:40 +0700

    Update backend dependencies

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post("/register", async (req, res) => {
     }
 
     await User.create(newUser);
-    console.log("Registered Users:", User);
+    console.log("Registered Users:", newUser);
 
     return res.status(201).json({
         message: "Registration successful",
@@ -132,5 +132,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
-    console.log(`Server is running on port${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
